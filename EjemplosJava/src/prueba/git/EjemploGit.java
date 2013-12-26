2e11be1a719b58107b9c7d7e41b29b7d001c0a81
Patonix <patricio.montecinos@gmail.com>
2013-12-26 20:29:33 -0300

    Update EjemploGit.java
    Prueba Github

--- EjemplosJava/src/prueba/git/EjemploGit.java
+++ EjemplosJava/src/prueba/git/EjemploGit.java
@@ -8,6 +8,7 @@ public class EjemploGit {
 		System.out.println("Prueba 1");
 		System.out.println("Prueba 2");
 		System.out.println("Se hace un cambio desde el depto");
+		System.out.println("Se hace un cambio desde el Github");
 	}
 	
 }
